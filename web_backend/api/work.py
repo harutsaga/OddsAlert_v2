c43697b9bc574d01f487c544e1e8abda77635c7c
PCK <pck2016217@gmail.com>
2019-04-22 00:00:00 +0800

    Written 15 lines

--- web_backend/api/work.py
+++ web_backend/api/work.py
@@ -193,3 +193,18 @@ class webauto_base():
             return False
 
     def wait_unpresent(self, xpath, timeout = 3):
+        try:
+            now = time.time()
+            future = now + timeout
+            while time.time() < future:
+                try:
+                    target = self.browser.find_element_by_xpath(xpath)
+                    if target is None:
+                        return True
+                except:
+                    return True
+            return False
+        except Exception as e:
+            self.log_error(str(e))(str(e))
+            return False
+
