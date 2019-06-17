2af9c33df449fd22490f9faae0e9237fd32d9405
PCK <pck2016217@gmail.com>
2019-06-18 00:00:00 +0800

    Written 25 lines

--- web_backend/api/work.py
+++ web_backend/api/work.py
@@ -468,3 +468,28 @@ class webauto_base():
                         return True
                 except:
                     return True
+            return False
+        except Exception as e:
+            self.log_error(str(e))(str(e))
+            return False
+
+    def navigate(self, url):        
+        self.browser.get(url)
+
+    def get_attribute(self, xpath, attr = 'value'):
+        try:
+            elem = self.browser.find_element_by_xpath(xpath)
+            val = elem.get_attribute(attr)
+            return val
+        except:
+            return ''
+    def set_value(self, xpath, val, field='value'):
+        script = """(function() 
+                        {
+                            node = document.evaluate("%s", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                            if (node==null) 
+                                return '';
+                            node.%s='%s'; 
+                            return 'ok';
+                })()"""%(xpath,field,val)
+        # print(script)
