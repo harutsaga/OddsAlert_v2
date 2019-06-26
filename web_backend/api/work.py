2dee4754aa9f6b3ee8ee3e868f171c7dace0e7e3
PCK <pck2016217@gmail.com>
2019-06-27 00:00:00 +0800

    Written 10 lines

--- web_backend/api/work.py
+++ web_backend/api/work.py
@@ -528,3 +528,13 @@ class webauto_base():
     def expand_shadow_element(self, element):
         try:
             shadow_root = self.browser.execute_script('return arguments[0].shadowRoot', element)
+# Abstract web automation class
+# 2019.09 David
+
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
