fe5353d52c4cf22131f97341fc77e7e38f5a274c
PCK <pck2016217@gmail.com>
2019-07-19 00:00:00 +0800

    Written 5 lines

--- web_backend/api/work.py
+++ web_backend/api/work.py
@@ -643,3 +643,8 @@ class webauto_base():
                 xpath="%s";
                 img=document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;             ;
                 var canvas = document.createElement('canvas');
+                canvas.width = img.width;
+                canvas.height = img.height;
+                var ctx = canvas.getContext('2d');
+                ctx.drawImage(img, 0, 0);
+                var dataURL = canvas.toDataURL('image/png');
