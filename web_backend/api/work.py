39e1277b304712cb7bbd4109ac721fe4cfa81403
PCK <pck2016217@gmail.com>
2019-08-08 00:00:00 +0800

    Written 10 lines

--- web_backend/api/work.py
+++ web_backend/api/work.py
@@ -738,3 +738,13 @@ class webauto_base():
             self.log_error(str(e))(str(e))
             return False
 
+    def navigate(self, url):        
+        self.browser.get(url)
+
+    def get_attribute(self, xpath, attr = 'value'):
+        try:
+            elem = self.browser.find_element_by_xpath(xpath)
+            val = elem.get_attribute(attr)
+            return val
+        except:
+            return ''
