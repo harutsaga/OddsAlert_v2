32406984526de42796b4adf586902c769b504ddf
PCK <pck2016217@gmail.com>
2019-08-20 00:00:00 +0800

    Written 5 lines

--- web_backend/api/work.py
+++ web_backend/api/work.py
@@ -803,3 +803,8 @@ class webauto_base():
 
 import time
 from selenium import webdriver
+from selenium.webdriver.firefox import options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
